e3a8f3dffc167e995fd6d60b1dd13e105c47e4b1
yokenkubo <yoken.kubo@ctc-g.co.jp>
2016-07-06 09:15:05 +0900

    tadasuke test

--- src/hello/routes/kamigou/eventList.js
+++ src/hello/routes/kamigou/eventList.js
@@ -4,6 +4,7 @@ var fs = require('fs');
 var router = express.Router();
 
 /* GET users listing. */
+/* tadasuke */
 router.get('/', function(req, res, next) {
 	res.render('kamigou/eventList', { title: 'ここからデータが渡せます。' });
 });
