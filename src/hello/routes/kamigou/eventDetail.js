9a274fc9c2aaeab70db02ec3d26a09bf01a56207
yokenkubo <yoken.kubo@ctc-g.co.jp>
2016-07-06 11:39:18 +0900

    イベント詳細のサーバサイド実装(平野さん)

--- src/hello/routes/kamigou/eventDetail.js
+++ src/hello/routes/kamigou/eventDetail.js
@@ -5,7 +5,16 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-	res.render('kamigou/eventDetail', { title: 'ここからデータが渡せます。' });
+	var file_name = req.query.file_name;
+    var msg = file_name == undefined ? '詳細ページ' : file_name;
+	res.render('kamigou/eventDetail', { title: msg, file_name: msg });
+});
+
+/* POST users listing. */
+router.post('/', function(req, res, next) {
+	var file_name = req.body['file_name'];
+	var msg = file_name == undefined ? '詳細page' : file_name;
+	res.render('kamigou/eventDetail', { title: msg, file_name: msg });
 });
 
 module.exports = router;
