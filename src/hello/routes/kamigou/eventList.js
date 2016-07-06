832945821d863f4c5853c5c379ade2cb764f7f12
yokenkubo <yoken.kubo@ctc-g.co.jp>
2016-07-06 09:37:00 +0900

    平井＿一覧画面サーバー側

--- src/hello/routes/kamigou/eventList.js
+++ src/hello/routes/kamigou/eventList.js
@@ -1,12 +1,48 @@
 var express = require('express');
 var qs = require('querystring');
 var fs = require('fs');
+var du = require('date-utils');
 var router = express.Router();
+var weekDayList = [ "日", "月", "火", "水", "木", "金", "土" ] ;
 
 /* GET users listing. */
-/* tadasuke */
 router.get('/', function(req, res, next) {
-	res.render('kamigou/eventList', { title: 'ここからデータが渡せます。' });
+	var dt = new Date();
+	var formatted = dt.toFormat("YYYYMMDDHH24MISS");
+	var reg=/(.*)(?:\.([^.]+$))/;
+	var resultJson = [];
+	var fileList = fs.readdirSync('data/event');
+
+	fileList.forEach(function(fileName) {
+		var targetYmd = fileName.match(reg)[1];
+		targetYmd = targetYmd.replace('event','');
+		if(targetYmd >= formatted) {
+			// try-catchでthrowして中断
+			try {
+				var strJson = fs.readFileSync('data/event/' + fileName, 'utf8');
+				var jsonData = JSON.parse(strJson);
+			} catch (e) {
+				var jsonData = [];
+			}
+			var dispDate = new Date();
+			dispDate.setFullYear(targetYmd.substr(0,4));
+			dispDate.setMonth(targetYmd.substr(4,2) - 1);
+			dispDate.setDate(targetYmd.substr(6,2));
+			dispDate.setHours(targetYmd.substr(8,2));
+			dispDate.setMinutes(targetYmd.substr(10,2));
+			var dispDateStr = dispDate.toFormat("YYYY/MM/DD") + '(' + weekDayList[dispDate.getDay()] + ')' + dispDate.toFormat("HH24:MI");
+			var obj = {
+				filename: fileName,
+				date: dispDateStr,
+				title: jsonData.title,
+				picture: jsonData.picture
+			};
+			resultJson.push(obj);
+		}
+	});
+
+	console.log(JSON.stringify(resultJson));
+	res.render('kamigou/eventList', JSON.stringify(resultJson, null, '    '));
 });
 
 module.exports = router;
