bc58309291f27c496ee5cd88d1bc599c98473efa
yokenkubo <yoken.kubo@ctc-g.co.jp>
2016-07-06 11:40:25 +0900

    イベント詳細の吹き出し

--- src/hello/public/javascripts/kamigou.js
+++ src/hello/public/javascripts/kamigou.js
@@ -33,8 +33,22 @@ MyMap = {
 	},
 
 	_putInfo: function(latlng) {
+
+		var inContents = '';
+		inContents += '<div id="contents">';
+		inContents +=   '<div id="imgL">';
+		inContents +=     '<a id="eventPicA" href="/images/Desert.jpg" data-lity>';
+		inContents +=       '<img id="eventPicImg" src="/images/Desert.jpg" alt="" />';
+		inContents +=     '</a>';
+		inContents +=     '<h4 id="eventDate">2016/07/07(木)</h3>';
+		inContents +=     '<h3 id="eventTitle">納涼祭</h3>';
+		inContents +=     '<p id="eventInfo">今年もやってきました！上郷ネオポリス納涼祭です。皆さんふるってご参加ください</p>';
+		inContents +=   '</div>';
+		inContents += '</div>';
+
 		var info = new google.maps.InfoWindow( {
-			content: "<span>吹き出しのメッセージ</span>",
+			content: inContents,
+//			content: "<span>吹き出しのメッセージ</span>",
 			pixelOffset: new google.maps.Size(0, -35), // 横方向に0, 上方向に10pxずらす
 		});
 
